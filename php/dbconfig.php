4880174f483b9b24abb938d33f8efefe69938c35
Monil Bid <bid.monil@gmail.com>
2018-03-17 20:15:53 -0400

    Made php scripts for db and sign up

---
+++ php/dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+	$HostName = "tempool-db-instance.cshxdqgs2sfj.us-east-2.rds.amazonaws.com";
+	 
+	$DatabaseName = "tempool_db";
+	 
+	$HostUser = "monilbid";
+	 
+	$HostPass = "monilbid";
+?>
